4637aedeecafb82bcf66fa4beb963bb65a74329b
nut-natthawat <natthawat48.noi@gmail.com>
2025-11-25 14:30:18 +0700

    Final Update

--- Projectmoblie/ContentView.swift
+++ Projectmoblie/ContentView.swift
@@ -4,12 +4,9 @@ struct ContentView: View {
     @EnvironmentObject var authManager: AuthManager
 
     var body: some View {
-        // เช็คสถานะ Login
         if let user = authManager.currentUser {
-            // --- State: LOGIN แล้ว -> ไปหน้า Tab Bar หลัก ---
             MainTabView(user: user)
         } else {
-            // --- State: ยังไม่ LOGIN -> ไปหน้า Login ---
             NavigationStack {
                 LoginView()
             }
